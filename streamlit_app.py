0d318f139df6604a49de85899ebff65d0daa2a8e
yebana <yebana@outlook.com>
2025-02-11 16:21:22 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,25 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Create a list of data to be represented in x-axis 
+days = [ 'Saturday' , 'Sunday' , 'Monday' , 'Tuesday' , 
+        'Wednesday' , 'Thursday' , 'Friday' ] 
+  
+# Create a list of data to be  
+# represented in y-axis 
+calories = [ 1670 , 2011 , 1853 , 2557 , 
+            1390 , 2118 , 2063 ] 
+  
+# Create a dataframe using the two lists 
+df_days_calories = pd.DataFrame( 
+    { 'day' : days , 'calories' : calories }) 
+  
+df_days_calories
+
+# use plot() method on the dataframe 
+df_days_calories.plot( 'day' , 'calories' ) 
+
+# Alternatively, you can use .set_index 
+# to set the data of each axis as follows: 
+# df_days_calories.set_index('day')['calories'].plot(); 
+
